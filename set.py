3192c8f583761fd3504e417fac45164b73d347ac
Anupama <anupamasingh1597@gmail.com>
2025-06-28 17:52:36 +0530

    python message

---
+++ set.py
@@ -0,0 +1,37 @@
+
+thisset = {"apple", "banana", "cherry","chikoo,"apple","mango"};
+print(thisset);
+print(len(thisset));
+print(type(myset));
+ for x in thisset:
+  print(x);
+thisset.add("orange")
+
+print(thisset)
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+thisset.remove("banana")
+thisset.pop();           
+
+print(thisset)           
+           
+           
+           
+           
+
+
+
+
+
+
+
+  
+
+
+
+
+
+  
